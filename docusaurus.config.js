5687479db78bd26b72c5914bc72fb8a052011606
Jose rey <jose_rrb_chivas@hotmail.com>
2025-01-10 15:11:03 -0700

    Update docusaurus config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,17 @@ const config = {
   favicon: 'img/logoKanji.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://joserey7.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Wiki-ShogunsReturn/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'joserey7', // Usually your GitHub org/user name.
   projectName: 'Wiki-ShogunsReturn', // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
